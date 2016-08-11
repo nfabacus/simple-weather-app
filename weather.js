569031b32e4555600e3ca601ffc83bff9410078f
noby <n.fujioka@abacusmaths.info>
2016-08-11 22:38:00 +0100

    code rewritten in a more oop way.

--- weather.js
+++ weather.js
@@ -1,43 +1,45 @@
-var tempMode;
-
-function Localweather() {
-  $.get( "http://api.wunderground.com/api/your-api-key-here/conditions/q/autoip.json", function( data ) {
+function LocalWeather() {
+    $.get( "http://api.wunderground.com/api/your-api-key-here/conditions/q/autoip.json", function( data ) {
     var city = data.current_observation.display_location.city;
+    var icon_url = data.current_observation.icon_url;
     var weather = "Sky is "+ data.current_observation.weather;
     var windspeed = "Windspeed: " + data.current_observation.wind_kph + " kph";
-    var icon_url = data.current_observation.icon_url;
     $('#city').html(city);
     console.log(icon_url);
     $('#weatherIcon').attr('src', icon_url);
     $('#weather').html(weather);
     $('#windspeed').html(windspeed);
-    getTemp();
   });
+  this.getTemp();
 }
+
+LocalWeather.prototype.toggleTemp = function() {
+    if(this.tempMode === "temp_c"){
+    this.tempMode = "temp_f";
+    this.degreeType = "\u00B0F";
+    this.degreeTo = "Celcius?";
+    $('#toggleTempBtn').html(this.degreeTo);
+  } else if (this.tempMode === "temp_f" || this.tempMode === undefined){
+    this.tempMode = "temp_c";
+    this.degreeType = "\u00B0C";
+    this.degreeTo = "Fahrenheit?";
+    $('#toggleTempBtn').html(this.degreeTo);
+  }
+  return [ this.tempMode, this.degreeType, this.degreeTo ];
+};
+
+LocalWeather.prototype.getTemp = function() {
+  $.get( "http://api.wunderground.com/api/your-api-key-here/conditions/q/autoip.json", function( data ) {
+    var tempArr = weather.toggleTemp();
+    var temperature = data.current_observation[tempArr[0]];
+    $('#temperature').html(temperature);
+    $('#degreeType').html(tempArr[1]);
+    $('#toggleTempBtn').html(tempArr[2]);
+  });
+};
+
+weather = new LocalWeather();
 
 $('#toggleTempBtn').click(function(){
-    getTemp();
+  weather.getTemp();
 });
-
-function getTemp() {
-  var degreeType;
-  var degreeTo;
-  if(tempMode === "temp_c"){
-    tempMode = "temp_f";
-    degreeType = "\u00B0F";
-    degreeTo = "Celcius?";
-    $('#toggleTempBtn').html('Celcius?');
-  } else if (tempMode === "temp_f" || tempMode === undefined){
-    tempMode = "temp_c";
-    degreeType = "\u00B0C";
-    degreeTo = "Fahrenheit?";
-  }
-  $.get( "http://api.wunderground.com/api/your-api-key-here/conditions/q/autoip.json", function( data ) {
-    var temperature = data.current_observation[tempMode];
-    $('#temperature').html(temperature);
-    $('#degreeType').html(degreeType);
-    $('#toggleTempBtn').html(degreeTo);
-  });
-}
-
-Localweather();
